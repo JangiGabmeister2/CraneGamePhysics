1de893284e5dbde5359e43a1d5bc63c78b47f2b2
Arjay Comabig <109714078+JangiGabmeister2@users.noreply.github.com>
2023-11-07 16:59:09 +1100

    resorted to working with localrotation

--- CraneGame/Assets/Scripts/CraneController.cs
+++ CraneGame/Assets/Scripts/CraneController.cs
@@ -6,17 +6,10 @@ public class CraneController : MonoBehaviour
 {
     public ConfigurableJoint crane, elevationLever, trainLever, rotationWheel;
 
-    public float jointValues1, jointValues2, jointValues3;
-
-    private void Start()
-    {
-        Mathf.Clamp(jointValues1, 0, 90);
-        Mathf.Clamp(jointValues2, 0, 90);
-        Mathf.Clamp(jointValues3, 145, -145);
-    }
+    public int jointValues1, jointValues2, jointValues3;
 
     private void Update()
     {
-
+        jointValues3 = (int)(rotationWheel.transform.localRotation.y * 90);
     }
 }
