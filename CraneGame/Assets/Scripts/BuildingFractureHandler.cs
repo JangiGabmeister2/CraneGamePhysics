21c8d4449e20c503be7cf17e80f3ecb835d38ad2
JangiGabmeister <109714078+JangiGabmeister2@users.noreply.github.com>
2024-01-05 11:33:53 +1100

    Updated project to include ragdolls
    Added ragdolls around building scene, that become ragdolls once the building beneath them collapses.
    Added construction worker model in crane booth for, like, immersion?

--- CraneGame/Assets/Scripts/BuildingFractureHandler.cs
+++ CraneGame/Assets/Scripts/BuildingFractureHandler.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 [RequireComponent(typeof(Fracture))]
 public class BuildingFractureHandler : MonoBehaviour
 {
-    private GameObject _homeWrecker;
     private Fracture _fracturer;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("WreckingBall"))
-            _homeWrecker = other.gameObject;
-
         _fracturer.CauseFracture();
     }
 }
